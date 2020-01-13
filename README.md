cf79200c7f04aaf0bbe38c1f88a06d55860992b7
beatjean <beatjean1314@gmail.com>
2020-01-13 16:11:15 +0800

    update

--- README.md
+++ README.md
@@ -3,3 +3,16 @@
 ## CVE-2019-6989
 
 `find ./ -name "httpd" -exec grep ipAddrDispose {} +`
+
+`find ./ -name "tplink_httpd_vs_*" | xargs rm -f`
+
+
+
+```shell
+find . -name "DIR_130_ssi_vs_*.BinDiff" > ./all_files.txt
+tar -T ./all_files.txt -zcvf ./ssi.tar.gz
+```
+
+
+
+`sudo pip install tensorflow==1.13.1 -i http://pypi.douban.com/simple/ --trusted-host pypi.douban.com`
--- bindiff-test/bindiff.py
+++ bindiff-test/bindiff.py
@@ -4,8 +4,22 @@ import os
 import time
 import sqlite3
 
-target_binary = "D:\\firmware\\tplink\\tplink_httpd.BinExport"
-do_ssc_addr = 0x0407068
+################################################################
+# target_binary = "D:\\firmware\\tplink\\tplink_httpd.BinExport"
+# target_func = 0x04703F0
+################################################################
+
+################################################################
+# target_binary = "D:\\firmware\\dlink\\busybox.BinExport"
+# target_func = 0x47AAC0
+################################################################
+
+################################################################
+target_binary = "D:\\firmware\\bug-search\\_US_AC9V1.0BR_V15.03.05.16\\squashfs-root\\bin\\httpd.BinExport"
+target_func = 0x00074550
+################################################################
+
+
 bindiff_exe = "D:\\software\\bindiff\\bin\\bindiff.exe"
 cnt = 0
 
@@ -15,7 +29,6 @@ def extract_record(db_name, func_addr):
 	db = sqlite3.connect(db_name)
 	cur = db.cursor()
 	result = cur.execute('select address2,similarity,confidence,algorithm from function where address1="{func}";'.format(func=func_addr))
-	# print(result.rowcount)
 	for row in result:
 		db.close()
 		return row
@@ -23,12 +36,15 @@ def extract_record(db_name, func_addr):
 def do_bindiff(secondary):
 	global target_binary
 	global bindiff_exe
+	global target_func
 	cmd = "{bindiff} --primary={primary} --secondary={secondary} --output_dir={output_dir}"
 	output_dir = os.path.dirname(secondary)
 	os.system(cmd.format(bindiff=bindiff_exe, primary=target_binary, secondary=secondary,output_dir=output_dir))
 
-	bindiff_file = os.path.basename(target_binary).split(".")[0] + "_vs_" + os.path.basename(secondary).split(".")[0] + ".BinDiff"
-	record = extract_record(os.path.join(output_dir, bindiff_file), do_ssc_addr)
+	target_basename = os.path.basename(target_binary)
+	sec_basename = os.path.basename(secondary)
+	bindiff_file = target_basename[:target_basename.rfind(".")] + "_vs_" + sec_basename[:sec_basename.rfind(".")] + ".BinDiff"
+	record = extract_record(os.path.join(output_dir, bindiff_file), target_func)
 	if record:
 		log_file.write(str(record)+";"+secondary+"\n")
 
